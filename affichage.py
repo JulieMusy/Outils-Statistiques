0ddd72b319cecc40ac53b0ed397ae3a4cd495084
Julie Musy <76889730+JulieMusy@users.noreply.github.com>
2021-02-12 13:43:41 +0100

    Create affichage.py

---
+++ affichage.py
@@ -0,0 +1,15 @@
+from matplotlib import collections  as mc
+import pylab as pl
+
+def afficher( liste ) :
+  mu , h , lines = sum(liste) / len(liste) , 0 , []
+  for élément in  liste :
+    lines.append( [ ( mu , h )  ,  ( élément , h ) ])
+    h += 1
+  muLine = mc.LineCollection([ [ ( mu , 0 )  , ( mu , len( notes )-1 )  ] ] , color = "red",  linewidths=2)
+  lc = mc.LineCollection(lines, color = "green",  linewidths=1)
+  fig, ax = pl.subplots()
+  ax.add_collection(lc)
+  ax.add_collection(muLine)
+  ax.autoscale()
+  ax.margins(0.1)
